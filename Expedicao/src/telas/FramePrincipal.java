8ff7a3abf03a6c1b9afd916b6382bf5e7aa3e895
Wesley Ricardo <Wesley Ricardo@DESKTOP-49HE1HQ>
2017-11-03 00:56:43 -0200

    Criado nos Internal Frames...

--- Expedicao/src/telas/FramePrincipal.java
+++ Expedicao/src/telas/FramePrincipal.java
@@ -25,8 +25,10 @@ public class FramePrincipal extends javax.swing.JFrame {
 
     private InternalPedido pedido = new InternalPedido();
     private InternalCargas cargas;
-    private InternalRotas rotas = new InternalRotas();
-
+    private InternalCadastroTransportador cadastrotransp = new InternalCadastroTransportador();
+    private InternalCadastroVeículos cadastroveiculos = new InternalCadastroVeículos();
+    private InternalNotaFiscal notafiscal = new InternalNotaFiscal();
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -47,8 +49,6 @@ public class FramePrincipal extends javax.swing.JFrame {
         cargasGerenciar = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenuItem5 = new javax.swing.JMenuItem();
-        expedicaoRotas = new javax.swing.JMenu();
-        rotasCriar = new javax.swing.JMenuItem();
 
         jMenu1.setText("jMenu1");
 
@@ -62,12 +62,17 @@ public class FramePrincipal extends javax.swing.JFrame {
         );
         painelPrincipalLayout.setVerticalGroup(
             painelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 768, Short.MAX_VALUE)
+            .addGap(0, 618, Short.MAX_VALUE)
         );
 
         Faturamento.setText("Faturamento");
 
-        jMenuItem4.setText("Notas Fiscais");
+        jMenuItem4.setText("Emissão de NFe");
+        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem4ActionPerformed(evt);
+            }
+        });
         Faturamento.add(jMenuItem4);
 
         barraMenu.add(Faturamento);
@@ -75,9 +80,19 @@ public class FramePrincipal extends javax.swing.JFrame {
         jMenu2.setText("Cadastrar");
 
         jMenuItem3.setText("Transportador");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
         jMenu2.add(jMenuItem3);
 
         jMenuItem2.setText("Veículos");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
         jMenu2.add(jMenuItem2);
 
         barraMenu.add(jMenu2);
@@ -97,18 +112,6 @@ public class FramePrincipal extends javax.swing.JFrame {
 
         barraMenu.add(cargasGerenciar);
 
-        expedicaoRotas.setText("Rotas");
-
-        rotasCriar.setText("Criar");
-        rotasCriar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                rotasCriarActionPerformed(evt);
-            }
-        });
-        expedicaoRotas.add(rotasCriar);
-
-        barraMenu.add(expedicaoRotas);
-
         setJMenuBar(barraMenu);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -123,21 +126,11 @@ public class FramePrincipal extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(painelPrincipal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 29, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void rotasCriarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rotasCriarActionPerformed
-
-        painelPrincipal.remove(rotas);
-        painelPrincipal.add(rotas);
-        rotas.setVisible(true);
-        rotas.setPosicao();
-
-
-    }//GEN-LAST:event_rotasCriarActionPerformed
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
 
@@ -148,6 +141,30 @@ public class FramePrincipal extends javax.swing.JFrame {
 
 
     }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        
+        painelPrincipal.remove(cadastrotransp);
+        painelPrincipal.add(cadastrotransp);
+        cadastrotransp.setVisible(true);
+        cadastrotransp.setPosicao();
+
+        
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        painelPrincipal.remove(cadastroveiculos);
+        painelPrincipal.add(cadastroveiculos);
+        cadastroveiculos.setVisible(true);
+        cadastroveiculos.setPosicao();
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
+        painelPrincipal.remove(notafiscal);
+        painelPrincipal.add(notafiscal);
+        notafiscal.setVisible(true);
+        notafiscal.setPosicao();
+    }//GEN-LAST:event_jMenuItem4ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -192,7 +209,6 @@ public class FramePrincipal extends javax.swing.JFrame {
     private javax.swing.JMenu Faturamento;
     private javax.swing.JMenuBar barraMenu;
     private javax.swing.JMenu cargasGerenciar;
-    private javax.swing.JMenu expedicaoRotas;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuItem jMenuItem1;
@@ -201,7 +217,6 @@ public class FramePrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JDesktopPane painelPrincipal;
-    private javax.swing.JMenuItem rotasCriar;
     // End of variables declaration//GEN-END:variables
 
     private Object getDesktopPane() {
